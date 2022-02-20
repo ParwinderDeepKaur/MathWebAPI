bd3194c02ab3e996a30e2e9cb84df4a643c78fe9
ParwinderDeepKaur <A00237487@mycambrian.ca>
2022-02-20 09:06:34 -0500

    added multiple test case(addition, subtraction, multiplication, division

--- MathTest/MathTest.cs
+++ MathTest/MathTest.cs
@@ -21,14 +21,21 @@ namespace MathTest
         /// <param name="value1"></param>
         /// <param name="value2"></param>
         [TestMethod]
-        [DataRow(2, 2)]
-        [DataRow(1, 3)]
-        [DataRow(4, 0)]
+        [DataRow(10,0 )]
+        [DataRow(9, 1)]
+        [DataRow(8, 2)]
+        [DataRow(3, 7)]
+        [DataRow(4, 6)]
+        [DataRow(5, 5)]
+        [DataRow(6, 4)]
+        [DataRow(7, 3)]
+        [DataRow(2, 8)]
+        [DataRow(0, 10)]
         public void IsAddTest(int value1,int value2)
         {
             
             // expected Result
-            var expected = 4;
+            var expected = 10;
 
             // Actual result
             var actual = _MathLogic.Add(value1, value2);
@@ -47,6 +54,13 @@ namespace MathTest
         [DataRow(3, 2)]
         [DataRow(4, 3)]
         [DataRow(6, 5)]
+        [DataRow(8, 7)]
+        [DataRow(10, 9)]
+        [DataRow(11, 10)]
+        [DataRow(14, 13)]
+        [DataRow(12, 11)]
+        [DataRow(15, 14)]
+        [DataRow(17, 16)]
         public void Subtract(int value1, int value2)
         {
 
@@ -70,6 +84,8 @@ namespace MathTest
         [DataRow(4, 5)]
         [DataRow(2, 10)]
         [DataRow(10, 2)]
+        [DataRow(20, 1)]
+        
         public void Multiply(int value1, int value2)
         {
 
@@ -93,6 +109,12 @@ namespace MathTest
         [DataRow(10, 5)]
         [DataRow(20, 10)]
         [DataRow(4, 2)]
+        [DataRow(50, 25)]
+        [DataRow(100, 50)]
+        [DataRow(200, 100)]
+        [DataRow(400, 200)]
+        [DataRow(16, 8)]
+        [DataRow(32, 16)]
         public void Division(int value1, int value2)
         {
 
